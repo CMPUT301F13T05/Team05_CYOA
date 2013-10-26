6a3b88c1d8c1c8c18f0d00a1d60bc5428700cdcd
varsis <chris@varsisstudio.com>
2013-10-26 15:40:30 -0600

    Added functions to web service controller started a test suit for the app

--- Adventure_app/gen/com/uofa/adventure_app/R.java
+++ Adventure_app/gen/com/uofa/adventure_app/R.java
@@ -68,15 +68,13 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_edit=0x7f05000a;
-        public static final int title_activity_edit_fragment=0x7f050005;
-        public static final int title_activity_fragement__edit=0x7f050006;
-        public static final int title_activity_new_story=0x7f050003;
-        public static final int title_activity_stroy=0x7f050004;
-        public static final int title_section1=0x7f050007;
-        public static final int title_section2=0x7f050008;
-        public static final int title_section3=0x7f050009;
+        public static final int title_activity_browser=0x7f050002;
+        public static final int title_activity_edit_fragement=0x7f050008;
+        public static final int title_activity_edit_story=0x7f050004;
+        public static final int title_activity_story=0x7f050003;
+        public static final int title_section1=0x7f050005;
+        public static final int title_section2=0x7f050006;
+        public static final int title_section3=0x7f050007;
     }
     public static final class style {
         /** 
---
+++ Adventure_app/src/com/uofa/adventure_app/controller/WebServiceController.java
@@ -0,0 +1,43 @@
+package com.uofa.adventure_app.controller;
+
+import java.util.ArrayList;
+
+import com.uofa.adventure_app.model.Story;
+
+public class WebServiceController {
+	
+/**
+ * Gets a story by name
+ * @return
+ */
+	public Story fetch(String name) {
+		
+	}
+	/**
+	 * Returns a story with a given id.
+	 * @param id
+	 * @return
+	 */
+	public Story fetch(Integer id) {
+		
+	}
+	
+	/**
+	 * Publishes the given story to the server
+	 * @param story
+	 * @return
+	 */
+	public boolean publish(Story story) {
+	
+	}
+	
+	/**
+	 * Searches the Server for the given key
+	 * @param searchKey
+	 * @return
+	 */
+	public ArrayList<Story> search(String searchKey) {
+		
+	}
+
+}
