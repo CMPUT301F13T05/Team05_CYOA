94736e985abfd1298e7bc53014ff625e52236b01
varsis <chris@varsisstudio.com>
2013-10-26 19:25:24 -0600

    Added Controllers to Application

--- Adventure_app/src/com/uofa/adventure_app/application/AdventureApplication.java
+++ Adventure_app/src/com/uofa/adventure_app/application/AdventureApplication.java
@@ -2,6 +2,29 @@ package com.uofa.adventure_app.application;
 
 import android.app.Application;
 
+import com.uofa.adventure_app.controller.StoryController;
+import com.uofa.adventure_app.controller.WebServiceController;
+
 public class AdventureApplication extends Application {
+	
+	transient private static StoryController storyController;
+	transient private static WebServiceController webServiceController;
+	
+	// Usable controller - singelton for all classes
+	public static StoryController getStoryController() {
+		if(storyController == null) {
+			storyController = new StoryController();
+		}
+		return storyController;	
+	}
+	
+	// Usable controller - singelton for all classes
+	public static WebServiceController getWebServiceController() {
+		if(webServiceController == null) {
+			webServiceController = new WebServiceController();
+		}
+		return webServiceController;	
+	}
+	
 
 }
