40358ba657fd94dbd70eb2be0fdaa5646854bc8f
varsis <chris@varsisstudio.com>
2013-11-02 16:00:09 -0600

    Added HandlerController

---
+++ Adventure_app/src/com/uofa/adventure_app/controller/HandlerController.java
@@ -0,0 +1,13 @@
+package com.uofa.adventure_app.controller;
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * This class should not be implemented
+ * @author chris
+ */
+public abstract class HandlerController {
+
+	
+	
+}
