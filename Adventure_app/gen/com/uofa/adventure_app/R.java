87494de531a154a09adc79373e9a552edf941e75
jmalina <jmalina@ualberta.ca>
2013-11-07 19:39:50 -0700

    Updating my folder

--- Adventure_app/gen/com/uofa/adventure_app/R.java
+++ Adventure_app/gen/com/uofa/adventure_app/R.java
@@ -24,44 +24,49 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int addmedia=0x7f080011;
-        public static final int annotatem=0x7f08001b;
-        public static final int annotation=0x7f08000d;
+        public static final int action_settings=0x7f080017;
+        public static final int addmedia=0x7f080014;
+        public static final int annotatem=0x7f08001f;
+        public static final int annotation=0x7f080010;
         public static final int authortext=0x7f080007;
-        public static final int authorview=0x7f08000a;
+        public static final int authorview=0x7f08000d;
         public static final int bchoice=0x7f080001;
         public static final int button1=0x7f080003;
-        public static final int cancel=0x7f080010;
-        public static final int choicesbutton=0x7f08000c;
-        public static final int choosemedia=0x7f08000f;
-        public static final int deletestory=0x7f08001a;
-        public static final int editfrag=0x7f080013;
-        public static final int editfragment=0x7f08001d;
-        public static final int editstory=0x7f08001c;
+        public static final int cancel=0x7f080013;
+        public static final int choicesbutton=0x7f08000f;
+        public static final int choosemedia=0x7f080012;
+        public static final int deletestory=0x7f08001e;
+        public static final int editfrag=0x7f080016;
+        public static final int editfragment=0x7f080021;
+        public static final int editstory=0x7f080020;
         public static final int fragments=0x7f080008;
         public static final int gridView1=0x7f080000;
-        public static final int new_story=0x7f080017;
+        public static final int new_story=0x7f08001b;
         public static final int newauthor=0x7f080004;
         public static final int newbody=0x7f080005;
-        public static final int newfragment=0x7f080019;
+        public static final int newfragment=0x7f08001d;
         public static final int newtitle=0x7f080002;
-        public static final int ok=0x7f080015;
-        public static final int publish=0x7f080012;
-        public static final int quit=0x7f08001e;
-        public static final int refresh=0x7f080018;
-        public static final int search=0x7f080016;
-        public static final int storyview=0x7f08000b;
-        public static final int takepic=0x7f08000e;
+        public static final int ok=0x7f080019;
+        public static final int okbutton=0x7f08000b;
+        public static final int publish=0x7f080015;
+        public static final int quit=0x7f080022;
+        public static final int refresh=0x7f08001c;
+        public static final int search=0x7f08001a;
+        public static final int storyview=0x7f08000e;
+        public static final int takepic=0x7f080011;
+        public static final int textView1=0x7f080009;
         public static final int titletext=0x7f080006;
-        public static final int titleview=0x7f080009;
-        public static final int username=0x7f080014;
+        public static final int titleview=0x7f08000c;
+        public static final int usern=0x7f08000a;
+        public static final int username=0x7f080018;
     }
     public static final class layout {
         public static final int activity_browser=0x7f030000;
         public static final int activity_edit_fragement=0x7f030001;
         public static final int activity_edit_story=0x7f030002;
-        public static final int activity_story=0x7f030003;
-        public static final int list_item=0x7f030004;
+        public static final int activity_first_run_only=0x7f030003;
+        public static final int activity_story=0x7f030004;
+        public static final int list_item=0x7f030005;
     }
     public static final class menu {
         public static final int annotatemenu=0x7f070000;
@@ -69,10 +74,11 @@ public final class R {
         public static final int createchoice=0x7f070002;
         public static final int edit=0x7f070003;
         public static final int editstorymenu=0x7f070004;
-        public static final int firstcontext=0x7f070005;
-        public static final int main=0x7f070006;
-        public static final int new_story=0x7f070007;
-        public static final int story=0x7f070008;
+        public static final int first_run_only=0x7f070005;
+        public static final int firstcontext=0x7f070006;
+        public static final int main=0x7f070007;
+        public static final int new_story=0x7f070008;
+        public static final int story=0x7f070009;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
