af43e64fa689766a350a1b10133b440282356704
varsis <chris@varsisstudio.com>
2013-11-30 17:17:17 -0700

    Annotations now working without images

--- Adventure_app/src/com/uofa/adventure_app/activity/AnnotateActivity.java
+++ Adventure_app/src/com/uofa/adventure_app/activity/AnnotateActivity.java
@@ -41,58 +41,58 @@ import com.uofa.adventure_app.application.AdventureApplication;
 import com.uofa.adventure_app.interfaces.AdventureActivity;
 import com.uofa.adventure_app.model.Annotation;
 import com.uofa.adventure_app.model.Story;
+
 /**
- * This class takes in user input and processes it when the user
- * is in the Annotate/Comment screen.  It interacts with the Annotation
- * class in the model. It also calls a camera and the internal storage
- * to allow the user to add pictures to their comment/annotation  
+ * This class takes in user input and processes it when the user is in the
+ * Annotate/Comment screen. It interacts with the Annotation class in the model.
+ * It also calls a camera and the internal storage to allow the user to add
+ * pictures to their comment/annotation
  * 
  * @author Ulvi Ibrahimov, Chris Pavlicek
- *
+ * 
  */
 
 public class AnnotateActivity extends AdventureActivity implements
-       OnItemClickListener {
+		OnItemClickListener {
 
-   ListView listView;
-   ArrayList<Annotation> rowItems;
-   Annotation newAnnotation = null;
+	ListView listView;
+	ArrayList<Annotation> rowItems;
+	Annotation newAnnotation = null;
+	boolean isNewAnnotation = false;
+	CustomListViewAdapter adapter;
 
-   /** Called when the activity is first created. */
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_annotate);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_annotate);
 
-       rowItems = new ArrayList<Annotation>();
-       rowItems = AdventureApplication.getStoryController().currentFragement().annotations();
+		rowItems = AdventureApplication.getStoryController().currentFragement().annotations();
+		System.out.println(rowItems);
+		listView = (ListView) findViewById(R.id.list);
+		adapter = new CustomListViewAdapter(this, R.layout.list_item, rowItems);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(this);
 
-       listView = (ListView) findViewById(R.id.list);
-       CustomListViewAdapter adapter = new CustomListViewAdapter(this,
-               R.layout.list_item, rowItems);
-       listView.setAdapter(adapter);
-       listView.setOnItemClickListener(this);
-       
-       EditText editTextView = (EditText) findViewById(R.id.editText);
-       
-       newAnnotation = new Annotation(AdventureApplication.user());
-       
-       //alertBox();
-   }
+		newAnnotation = new Annotation(AdventureApplication.user());
 
-   @Override
+		// alertBox();
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.annotatemenu, menu);
 		return super.onCreateOptionsMenu(menu);
-   }
-   
+	}
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle presses on the action bar items
 		switch (item.getItemId()) {
 		case R.id.newAnnotation:
+			isNewAnnotation = true;
 			alertBox();
 			break;
 		case R.id.choosemedia:
@@ -106,82 +106,117 @@ public class AnnotateActivity extends AdventureActivity implements
 		}
 		return super.onOptionsItemSelected(item);
 	}
-   
+
 	public void alertBox() {
 
-		 final Dialog dialog = new Dialog(this,R.style.DialogTheme);
-		 LayoutInflater inflater = this.getLayoutInflater();
-		 dialog.setTitle("Create an Annotation");
-		 View alertView = inflater.inflate(R.layout.annotate_alert, null);
-	        
-        TextView authorText = (TextView)alertView.findViewById(R.id.alertTitleAnnotation);
-        authorText.setText("User: " + AdventureApplication.user().getName());
-        
-        EditText editTextView = (EditText) alertView.findViewById(R.id.editText);
-        editTextView.addTextChangedListener(new GenericTextWatcher(editTextView));
-        
-        alertView.findViewById(R.id.cancelAnnotation).setOnClickListener(
-        		new Button.OnClickListener() {  
-        	        public void onClick(View v)
-        	            {
-        	                dialog.dismiss();
-        	            }});
-        	            
-       alertView.findViewById(R.id.okAnnotation).setOnClickListener(
-        	            		new Button.OnClickListener() {  
-        	            	        public void onClick(View v)
-        	            	            {
-        	            	                AdventureApplication.getStoryController().currentFragement().addAnnotation(newAnnotation);
-        	            	    }});
-        
-        
-        
-        dialog.setContentView(alertView);
-    	dialog.show();
-       
-    	
-    	
-	}
-	
-   @Override
-   public void onItemClick(AdapterView<?> parent, View view, int position,
-           long id) {
-       Toast toast = Toast.makeText(getApplicationContext(),
-           "Item " + (position + 1) + ": " + rowItems.get(position),
-           Toast.LENGTH_SHORT);
-       toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
-       toast.show();
-   }
+		final Dialog dialog = new Dialog(this, R.style.DialogTheme);
+		LayoutInflater inflater = this.getLayoutInflater();
+		dialog.setTitle("Create an Annotation");
+		View alertView = inflater.inflate(R.layout.annotate_alert, null);
 
-@Override
-public void dataReturn(ArrayList<Story> result, String method) {
-	// TODO Auto-generated method stub
-	
-}
+		TextView authorText = (TextView) alertView
+				.findViewById(R.id.alertTitleAnnotation);
+		authorText.setText("User: " + AdventureApplication.user().getName());
 
-@Override
-public void updateView() {
-	// TODO Auto-generated method stub
-	
-}
+		EditText editTextView = (EditText) alertView
+				.findViewById(R.id.editText);
+		editTextView.setText(newAnnotation.annotationString());
+		editTextView
+				.addTextChangedListener(new GenericTextWatcher(editTextView));
 
-@Override
-protected void openLastFragement() {
-	// TODO Auto-generated method stub
-	
-}
+		alertView.findViewById(R.id.cancelAnnotation).setOnClickListener(
+				new Button.OnClickListener() {
+					public void onClick(View v) {
+						dialog.dismiss();
+					}
+				});
 
-@Override
-protected void saveTextForView(View v, String text) {
+		alertView.findViewById(R.id.okAnnotation).setOnClickListener(
+				new Button.OnClickListener() {
+					public void onClick(View v) {
+						saveAnnotation();
+						dialog.dismiss();
+					}
+				});
 
-	switch (v.getId()) {
-	case R.id.editText:
-		newAnnotation.setAnnotationString(text);
-	default:
-		break;
+		alertView.findViewById(R.id.chooseMedia).setOnClickListener(
+				new Button.OnClickListener() {
+					public void onClick(View v) {
+						addMedia();
+					}
+				});
+
+		alertView.findViewById(R.id.takePicture).setOnClickListener(
+				new Button.OnClickListener() {
+					public void onClick(View v) {
+						takePicture();
+					}
+				});
+
+		dialog.setContentView(alertView);
+		dialog.show();
+
 	}
 
-	AdventureApplication.getActivityController().update();
+	public void addMedia() {
 
-}
+	}
+
+	public void takePicture() {
+
+	}
+
+	public void saveAnnotation() {
+		if(isNewAnnotation) {
+		AdventureApplication.getStoryController().currentFragement()
+				.addAnnotation(newAnnotation);
+		} else {
+			AdventureApplication.getStoryController().currentFragement()
+			.replaceAnnotation(newAnnotation);
+		}
+		isNewAnnotation = false;
+		AdventureApplication.getStoryController().saveStories();
+		newAnnotation = new Annotation(AdventureApplication.user());
+		this.updateView();
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		newAnnotation = rowItems.get(position);
+		if(newAnnotation.user().equals(AdventureApplication.user())) {
+			alertBox();
+		}
+	}
+
+	@Override
+	public void dataReturn(ArrayList<Story> result, String method) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateView() {
+		adapter.notifyDataSetChanged();
+	}
+
+	@Override
+	protected void openLastFragement() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void saveTextForView(View v, String text) {
+
+		switch (v.getId()) {
+		case R.id.editText:
+			newAnnotation.setAnnotationString(text);
+		default:
+			break;
+		}
+
+		AdventureApplication.getActivityController().update();
+
+	}
 }
